10e89fdc027b04e5f1cdb17fa7e83f447e2c9bda
Olaolu Olawuyi <mrolaolu@gmail.com>
2018-08-21 11:25:44 +0100

    🦄: add option to enable preceding zeros
    closes #3

--- src/index.js
+++ src/index.js
@@ -24,12 +24,14 @@ function reverse(input, options = {}) {
   const globArr = [...input].reverse()
   , then = options.then
   , minusRE = /^-/
+    enforceZeros = options.enforceZeros,
 
   if (input && !supported(input))
     throw new TypeError('Failed to apply \'reverse\': ' + kindof(input) + 's are not supported')
 
   options.invert = options.invert || 'index'
   options.then = then || ( (_, v) => v )
+  options.enforceZeros = enforceZeros || false;
 
   let result
   switch ( kindof(input) ) {
@@ -43,18 +45,41 @@ function reverse(input, options = {}) {
     case 'number':
       // convert the number to string then replace the minus(-) symbol with nothing
       const nStr = String(input).replace(minusRE, '')
+      const containsTrailingZeros = /0+$/.test(positiveNumberAsString);
 
       if (/e/.test(nStr)) throw new TypeError('Oops. That number is too large. See https://github.com/whizkydee/type-reverse/blob/master/readme.md#limits for more info.')
+      // there's no need to stringify if...
+      // 1. the input contains trailing zeros, but the enforceZeros option is disabled
+      // 2. the input doesn't contain trailing zeros, but the enforceZeros option is enabled
+      // 3. the input doesn't contain trailing zeros, and the enforceZeros option is disabled
+      const noNeedToStringify =
+        (containsTrailingZeros && !enforceZeros) ||
+        (!containsTrailingZeros && enforceZeros) ||
+        (!containsTrailingZeros && !enforceZeros);
 
       switch (options.invert) {
         case 'sign':
-          result = ( minusRE.test(input) ) ? Number(+nStr) : Number(-nStr)
-        break
+          if (noNeedToStringify) {
+            result = minusRE.test(input)
+              ? Number(+positiveNumberAsString)
+              : Number(-positiveNumberAsString);
+          } else {
+            result = minusRE.test(input)
+              ? positiveNumberAsString
+              : '-' + positiveNumberAsString;
+          }
+          break;
         case 'index':
-          result = ( minusRE.test(input) ) ?
-            reverse(nStr, { then: (_, x) => Number(-x) }) :
-            reverse(nStr, { then: (_, x) => Number(x) })
-        break
+          if (noNeedToStringify) {
+            result = minusRE.test(input)
+              ? reverse(positiveNumberAsString, null, (_, x) => Number(-x))
+              : reverse(positiveNumberAsString, null, (_, x) => Number(x));
+          } else {
+            result = minusRE.test(input)
+              ? reverse(positiveNumberAsString, null, (_, x) => '-' + x)
+              : reverse(positiveNumberAsString, null, (_, x) => x);
+          }
+          break;
       }
     break
 
