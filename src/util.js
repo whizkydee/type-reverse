2db35f3e2366f24a876004e8adc1adf945be5648
Olaolu Olawuyi <mrolaolu@gmail.com>
2018-08-21 11:56:01 +0100

    🦄: cleanup util file

--- src/util.js
+++ src/util.js
@@ -1,6 +1,6 @@
 // A smarter `typeof` function
 const kindof = v =>
-  v === undefined ? 'undefined' : v === null ? 'null' : v.constructor.name.toLowerCase()
+  v === undefined ? 'undefined' : v === null ? 'null' : v.constructor.name.toLowerCase();
 
 const is = {
   set: i => kindof(i) === 'set',
@@ -8,8 +8,8 @@ const is = {
   string: i => kindof(i) === 'string',
   number: i => kindof(i) === 'number',
   nodelist: i => kindof(i) === 'nodelist'
-}
+};
 
-const supported = i => is.string(i) || is.number(i) || is.nodelist(i) || is.array(i) || is.set(i)
+const supported = i => is.string(i) || is.number(i) || is.nodelist(i) || is.array(i) || is.set(i);
 
-export { kindof, supported }
+export { kindof, supported };
